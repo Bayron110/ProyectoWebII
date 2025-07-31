23c449616f8a9632eb962601f1a099ec8fe9f6f5
Bayron110 <bayron124z@hotmail.com>
2025-07-30 22:59:30 -0500

    VersionStyles

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Carusel } from "../../components/carusel/carusel";
+import { Noticias } from "../../components/noticias/noticias";
 
 @Component({
   selector: 'app-home',
-  imports: [Carusel],
+  imports: [Carusel, Noticias],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
